4043597cd93d13d38ae6f487038bd1efe8ef3c08
Javier Aristóteles Prieto Flores <75401043+jvi326@users.noreply.github.com>
2025-11-03 01:17:31 -0600

    Codigo para ESP32, detección de distancia

---
+++ ESP_32_medidas_ultrasonic/globals.h
@@ -0,0 +1,12 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <stdint.h>
+
+extern float threshold_cm;  // declarada como extern para acceso global
+extern bool new_threshold;  // declarada flag para acceso global, cambio de valor threshold
+
+extern bool ext_trig1;  // declarada flag para acceso global, cambio de valor threshold
+extern bool ext_trig2;  // declarada flag para acceso global, cambio de valor threshold
+
+#endif
