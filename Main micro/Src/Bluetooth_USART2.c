2ddc8c453f338ae4e22d32b40f9d8dd31746c466
Javier Aristóteles Prieto Flores <75401043+jvi326@users.noreply.github.com>
2025-11-03 02:03:54 -0600

    Se elimino info de debug por usart, para mandar info a la app

--- Main micro/Src/Bluetooth_USART2.c
+++ Main micro/Src/Bluetooth_USART2.c
@@ -96,8 +96,8 @@ void USART2_IRQHandler(void){
 void USART2_HandleMessage(CHASSIS* AGV_Chassis) {
 
 
-	USART2_SendString("Echo: ");
-	USART2_SendString((char*)rx_buf);
+	//USART2_SendString("Echo: ");
+	//USART2_SendString((char*)rx_buf);
 
 	uint8_t numArrays = parseCSV(rx_buf, rx_pos, parsedArrays);
 	uint8_t numArrayIntFloat = arrayToArrayIntOrFloat(parsedArrays, numArrays, indicacionesArray);
@@ -381,10 +381,10 @@ uint8_t arrayToArrayIntOrFloat(volatile NumberArray* numbersAsArray, uint8_t num
 
 	// Process the parsed arrays
 	for (uint8_t i = 0; i < numberOfArrays; i++) {
-		USART2_SendString("Number ");
-		USART2_SendChar('0' + i);
-		USART2_SendString(": ");
-		USART2_SendString((const char*)numbersAsArray[i].array);
+		//USART2_SendString("Number ");
+		//USART2_SendChar('0' + i);
+		//USART2_SendString(": ");
+		//USART2_SendString((const char*)numbersAsArray[i].array);
 
 		numero = atof(numbersAsArray[i].array, numbersAsArray[i].length);
 
